7c74c2aa8ef4b41a76432c680def67f6ebb8a642
Pranit Dhage <pranitd@cybage.com>
2021-09-19 15:35:54 +0530

    Get User list, Toggle user active status Completed

--- admin-panel/src/app/components/user-list/user-list.component.ts
+++ admin-panel/src/app/components/user-list/user-list.component.ts
@@ -1,15 +1,36 @@
+import { UserService } from './../../services/user.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
-  styleUrls: ['./user-list.component.css']
+  styleUrls: ['./user-list.component.css'],
 })
 export class UserListComponent implements OnInit {
-
-  constructor() { }
+  users: any[] = [];
+  constructor(private userService: UserService) {}
 
   ngOnInit(): void {
+    this.loadUsers();
   }
 
+  loadUsers() {
+    this.userService.getUsers().subscribe((response: any) => {
+      if (response['status'] == 'success') {
+        this.users = response['data'];
+      } else {
+        console.log(response['error']);
+      }
+    });
+  }
+
+  toggleActive(user: any) {
+    this.userService.toggelActiveStatus(user).subscribe((response: any) => {
+      if (response['status'] == 'success') {
+        this.loadUsers();
+      } else {
+        console.log(response['error']);
+      }
+    });
+  }
 }
--- admin-panel/src/app/services/user.service.ts
+++ admin-panel/src/app/services/user.service.ts
@@ -1,9 +1,37 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
+  url = 'http://localhost:3000/user';
+  constructor(private httpClient: HttpClient) {}
 
-  constructor() { }
+  getUsers() {
+    //Add token in request header
+    const httpOptions = {
+      headers: new HttpHeaders({
+        token: sessionStorage['token'],
+      }),
+    };
+    return this.httpClient.get(this.url, httpOptions);
+  }
+  toggelActiveStatus(user: any) {
+    //Add token in request header
+    const httpOptions = {
+      headers: new HttpHeaders({
+        token: sessionStorage['token'],
+      }),
+    };
+    const body = {
+      status: user['active'] == 1 ? 0 : 1,
+    };
+
+    return this.httpClient.put(
+      this.url + '/toggle-active/' + user['id'],
+      body,
+      httpOptions
+    );
+  }
 }
